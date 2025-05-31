ec7ffac3a9b7e4914aab39ef5c7d89787855acb5
Paulo B F Almeida <paulobfalmeida@gmail.com>
2025-05-31 01:45:20 -0300

    update arvores

--- Scripts/Ferramentas/Ferramenta.gd
+++ Scripts/Ferramentas/Ferramenta.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 
 @export_flags_2d_physics var layer_acao : int
 
-enum Ferramenta_tipo {CORTAR, PLANTAR, LIXO, NONE}
+enum Ferramenta_tipo {CORTAR, PLANTAR, RECOLHER, NONE}
 
 @export var tipo_ferramenta : Ferramenta_tipo
 
--- Scripts/Partida/arvore_mangue.gd
+++ Scripts/Partida/arvore_mangue.gd
@@ -3,9 +3,17 @@ extends Arvore
 @onready var timer_crescer := $TimerCrescer
 
 @export var sprites_idade : Dictionary[Crescimento, Sprite2D] = {}
+@export var collisions_idade : Dictionary[Crescimento, CollisionShape2D] = {}
 
 func _ready() -> void:
 	comecar_crescer()
+	# desligar as colisoes e sprites
+	for key in sprites_idade.keys():
+		sprites_idade[key].hide()
+		collisions_idade[key].hide()
+		collisions_idade[key].disabled = true
+	# ligar as colisoes e sprites atuais
+	_update_sprite(true)
 
 func comecar_crescer() -> void:
 	super.comecar_crescer()
@@ -13,13 +21,24 @@ func comecar_crescer() -> void:
 	timer_crescer.start(crescimento_time)
 
 func crescer() -> void:
-	sprites_idade[idade].hide()
+	_update_sprite(false) # esconde a sprite antiga (antes de atualizar a idade)
 	super.crescer()
-	sprites_idade[idade].show()
+	_update_sprite(true) # mostra a sprite nova (dps de atualizar a idade)
 
 	if idade == Crescimento.ADULTA:
 		timer_crescer.stop()
 
+## mostrar == true -> aparece a sprite, mostrar == false -> esconde 
+func _update_sprite(mostrar : bool) -> void:
+	if not mostrar:
+		sprites_idade[idade].hide()
+		collisions_idade[idade].hide()
+		collisions_idade[idade].disabled = true
+	else:
+		sprites_idade[idade].show()
+		collisions_idade[idade].show()
+		collisions_idade[idade].disabled = false
+		
 
 func _on_timer_crescer_timeout() -> void:
 	crescer()
