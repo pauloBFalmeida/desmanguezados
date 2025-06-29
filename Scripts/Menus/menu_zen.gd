be383df2c22a236e59f09f0252c6d410f75671af
Paulo B F Almeida <paulobfalmeida@gmail.com>
2025-06-29 04:31:22 -0300

    menu zen atualizado
    com poder escolher a seed

--- Scripts/Menus/menu_zen.gd
+++ Scripts/Menus/menu_zen.gd
@@ -1,9 +1,17 @@
 extends Menu
 
 @onready var button_foco := $GridContainer/Button1Jog
+@onready var text_seed := $TextEditSeed
 
 func _ready() -> void:
 	button_foco.grab_focus()
+	# cria uma seed aleatoria
+	Globais.modo_zen_mapa_seed = randi_range(0, 99999)
+	text_seed.text = str(Globais.modo_zen_mapa_seed)
+
+func comecar_partida() -> void:
+	SceneManager.goto_level(Globais.Level_id.ZEN)
+	Globais.modo_zen_mapa_seed = int(text_seed.text)
 
 # --- Voltar ---
 func _on_button_voltar_pressed() -> void:
@@ -12,8 +20,8 @@ func _on_button_voltar_pressed() -> void:
 # --- Botoes quantidade de jogadores ---
 func _on_button_1_jog_pressed() -> void:
 	Globais.modo_zen_ter_1_jogador = true
-	SceneManager.goto_level(Globais.Level_id.ZEN)
+	comecar_partida()
 
 func _on_button_2_jog_pressed() -> void:
 	Globais.modo_zen_ter_1_jogador = false
-	SceneManager.goto_level(Globais.Level_id.ZEN)
+	comecar_partida()
