0de3c6e9f3cb1af32c64466f9e23dfa08570b4bd
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-03-31 03:50:20 -0300

    pegar as configuracoes da conexao
    pega o IPv4 Interno da rede
    E adicionado link para página sobre multiplayer em godot 4x

--- Scripts/Menus/menu_online.gd
+++ Scripts/Menus/menu_online.gd
@@ -6,7 +6,9 @@ extends Menu
 @onready var button_join: Button = $VBox/HBoxJog/ButtonJoin
 
 @onready var text_ip: LineEdit = $VBox/GridContainer/TextIP
-@onready var spin_box_port: SpinBox = $VBox/GridContainer/SpinBoxPort
+@onready var text_port: SpinBox = $VBox/GridContainer/SpinBoxPort
+
+var is_host : bool = true
 
 # --- Voltar ---
 func _on_button_voltar_pressed() -> void:
@@ -15,27 +17,38 @@ func _on_button_voltar_pressed() -> void:
 func _ready() -> void:
 	button_host.grab_focus()
 	# ---
+	text_ip.placeholder_text = _get_local_ipv4()
+	update_button_server_visual()
 
 func _on_button_comecar_pressed() -> void:
 	pass # Replace with function body.
 
 
-# --- Botoes quantidade de jogadores ---
+func _get_local_ipv4() -> String:
+	for address in IP.get_local_addresses():
+		# pega enderecos IP v4
+		if (address.split('.').size() == 4):
+			# IP interno, normalmente na forma 192.168.*.*
+			if address.contains("192.168"):
+				return address
+	# IP para localhost, ambos jogos rodando no mesmo PC
+	return "127.0.0.1"
+
+# --- Botoes Host e Join ---
 func update_button_server_visual() -> void:
-	if host:
+	if is_host:
 		button_host.set_pressed_no_signal(true)
 		button_join.set_pressed_no_signal(false)
+		text_ip.editable = false
 	else:
 		button_host.set_pressed_no_signal(false)
 		button_join.set_pressed_no_signal(true)
-	
-
-var host := true
+		text_ip.editable = true
 
 func _on_button_host_pressed() -> void:
-	host = true
+	is_host = true
 	update_button_server_visual()
 
 func _on_button_join_pressed() -> void:
-	host = false
+	is_host = false
 	update_button_server_visual()
