4979359dd21a295b359e92dc04ef2b0201f04b3d
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-26 01:36:21 -0300

    Clean code: Scene manager
    adicionado retorno da função, a simplificado instanciar cena

--- Scripts/Autoloads/sceneManager.gd
+++ Scripts/Autoloads/sceneManager.gd
@@ -13,31 +13,31 @@ const MENU_INFORMACOES_PATH 	:= "uid://icyktkxgwl6u"
 func goto_menu():
 	change_scene(MENUS_PATH)
 
-func goto_menu_principal():
+func goto_menu_principal() -> void:
 	change_menu(MENU_PRINCIPAL_PATH)
 
-func goto_selecao():
+func goto_selecao() -> void:
 	change_menu(MENU_SELECAO_PATH)
 
-func goto_online():
+func goto_online() -> void:
 	change_menu(MENU_ONLINE_PATH)
 
-func goto_menu_zen():
+func goto_menu_zen() -> void:
 	change_menu(MENU_MENU_ZEN_PATH)
 
-func goto_configuracoes():
+func goto_configuracoes() -> void:
 	change_menu(MENU_CONFIGURACAO_PATH)
 
-func goto_creditos():
+func goto_creditos() -> void:
 	change_menu(MENU_CREDITOS_PATH)
 
-func goto_stats():
+func goto_stats() -> void:
 	change_menu(MENU_ESTATISTICAS_PATH)
 
-func goto_info():
+func goto_info() -> void:
 	change_menu(MENU_INFORMACOES_PATH)
 
-func goto_level(level_id : LevelManager.Level_id):
+func goto_level(level_id : LevelManager.Level_id) -> void:
 	if LevelManager.LEVEIS_REF.has(level_id):
 		Globais.current_level_id = level_id
 		change_scene(LevelManager.LEVEIS_REF[level_id])
@@ -46,25 +46,27 @@ func restart_level() -> void:
 	goto_level(Globais.current_level_id)
 
 # ------  ---------
-func change_scene(path: String):
+func change_scene(path: String) -> void:
 	# Clean up current scene
 	var current_scene = get_tree().current_scene
 	if current_scene:
 		current_scene.queue_free()
 	
 	# Load new scene
-	var scene_ref = load(path)
-	var new_scene = scene_ref.instantiate()
+	var new_scene = _load_scene(path)
 	get_tree().root.add_child(new_scene)
 	get_tree().current_scene = new_scene
 
-func change_menu(path: String):
+func change_menu(path: String) -> void:
 	var main_menus = get_tree().root.get_node("MainMenus")
 	# Clean up current scene
 	for child in main_menus.get_children():
 		child.queue_free()
 	
 	# Load new scene
+	main_menus.add_child(_load_scene(path))
+
+func _load_scene(path: String) -> Node:
 	var scene_ref = load(path)
 	var new_scene = scene_ref.instantiate()
-	main_menus.add_child(new_scene)
+	return new_scene
