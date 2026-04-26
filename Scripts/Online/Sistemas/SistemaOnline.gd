434cf32140a2cb8688004ffc6b8e44af38667441
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-25 21:10:56 -0300

    Clean code: Sistema online comentários explicativos

--- Scripts/Online/Sistemas/SistemaOnline.gd
+++ Scripts/Online/Sistemas/SistemaOnline.gd
@@ -7,9 +7,10 @@ func _init() -> void:
 		queue_free()
 
 func _ready() -> void:
+	# espera 1 frame antes de comecar a config online
 	await get_tree().process_frame
-	
 	iniciar_online_config()
 
+## Iniciar as configuracoes do Sistema Online, apos o primeiro frame
 func iniciar_online_config() -> void:
 	pass
