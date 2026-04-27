69aac7cae5cfccbc94c40d16ea052df6e9353e51
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-27 03:13:07 -0300

    Update add controle no modo online
    Agora adiciona o controle pro siri que o jogador esta controlando no modo online

--- Scripts/Menus/UsadosMenus/controles_manager.gd
+++ Scripts/Menus/UsadosMenus/controles_manager.gd
@@ -6,7 +6,21 @@ func _input(event):
 		var device_id = event.device
 		# se o controle nao foi adicionado ainda
 		if not InputManager.controles_conectados.has(device_id):
-			# Adiciona primeiro o controle do P2, dps o do P1
-			var player_id := InputManager.PlayerId.P2 if InputManager.controles_conectados.is_empty() else InputManager.PlayerId.P1
-			# adiciona o inputmap pro controle
-			InputManager.add_controller(player_id, device_id)
+			# adiciona o controle
+			_add_controller(device_id)
+
+## Associa o controle para um dos jogadores
+func _add_controller(device_id: int) -> void:
+	# Decide pra qual jogar vai o controle
+	var player_id := _decidir_jogador()
+	# adiciona o inputmap pro controle
+	InputManager.add_controller(player_id, device_id)
+
+## Decide pra qual jogar vai o controle
+func _decidir_jogador() -> InputManager.PlayerId:
+	if NetworkingGame.is_game_online:
+		# adiciona controle pro siri que o jogador esta controlando
+		return NetworkingGame.jogador_player_id
+	else:
+		# Adiciona primeiro o controle do P2, dps o do P1
+		return InputManager.PlayerId.P2 if InputManager.controles_conectados.is_empty() else InputManager.PlayerId.P1
