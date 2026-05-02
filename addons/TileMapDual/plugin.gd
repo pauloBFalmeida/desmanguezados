fda2b089456556932f8b160624bb4afdb2c75bdf
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-05-02 00:32:14 -0300

    update tile map dual

--- addons/TileMapDual/plugin.gd
+++ addons/TileMapDual/plugin.gd
@@ -2,25 +2,35 @@
 class_name TileMapDualEditorPlugin
 extends EditorPlugin
 
-static var instance: TileMapDualEditorPlugin = null
-
 
 # TODO: create a message queue that groups warnings, errors, and messages into categories
 # so that we don't get 300 lines of the same warnings pushed to console every time we undo/redo
-
-
 func _enter_tree() -> void:
-	# assign singleton instance
-	instance = self
 	# register custom nodes
-	add_custom_type("TileMapDual", "TileMapLayer", preload("TileMapDual.gd"), preload("TileMapDual.svg"))
-	add_custom_type("CursorDual", "Sprite2D", preload("CursorDual.gd"), preload("CursorDual.svg"))
-	add_custom_type("TileMapDualLegacy", "TileMapLayer", preload("TileMapDualLegacy.gd"), preload("TileMapDual.svg"))
+	const TILE_MAP_DUAL_ICON: Texture2D = preload("tile_map_dual.svg")
+	add_custom_type(
+		"TileMapDual",
+		"TileMapLayer",
+		preload("tile_map_dual.gd"),
+		TILE_MAP_DUAL_ICON,
+	)
+	add_custom_type(
+		"TileMapDualLegacy",
+		"TileMapLayer",
+		preload("tile_map_dual_legacy.gd"),
+		TILE_MAP_DUAL_ICON,
+	)
+	add_custom_type(
+		"CursorDual",
+		"Sprite2D",
+		preload("cursor_dual.gd"),
+		preload("cursor_dual.svg"),
+	)
 	# load editor-only functions
 	TileMapDual.autotile = autotile
 	TileMapDual.popup = popup
 	# finish
-	print("plugin TileMapDual loaded")
+	# print("plugin TileMapDual loaded")
 
 
 func _exit_tree() -> void:
@@ -31,16 +41,15 @@ func _exit_tree() -> void:
 	remove_custom_type("TileMapDualLegacy")
 	remove_custom_type("CursorDual")
 	remove_custom_type("TileMapDual")
-	# unassign singleton instance
-	instance = null
 	# finish
-	print("plugin TileMapDual unloaded")
+	# print("plugin TileMapDual unloaded")
 
 
-# HACK: functions that reference EditorPlugin, directly or indirectly, cannot be in the publicly exported scripts
+# HACK: functions that reference EditorPlugin, directly or indirectly,
+# cannot be in the publicly exported scripts
 # or else they simply won't work when exported
-
-## Shows a popup with a title bar, a message, and an "Ok" button in the middle of the screen.
+## Shows a popup with a title bar, a message,
+## and an "Ok" button in the middle of the screen.
 func popup(title: String, message: String) -> void:
 	var popup := AcceptDialog.new()
 	get_editor_interface().get_base_control().add_child(popup)
@@ -53,11 +62,17 @@ func popup(title: String, message: String) -> void:
 
 
 ## Automatically generate terrains when the atlas is initialized.
-func autotile(source_id: int, atlas: TileSetAtlasSource, tile_set: TileSet):
+func autotile(_source_id: int, atlas: TileSetAtlasSource, tile_set: TileSet):
 	print_stack()
-	var urm := get_undo_redo()
-	urm.create_action("Create tiles in non-transparent texture regions", UndoRedo.MergeMode.MERGE_ALL, self, true)
+	var urm: EditorUndoRedoManager = get_undo_redo()
+	urm.create_action(
+		"Create tiles in non-transparent texture regions",
+		UndoRedo.MergeMode.MERGE_ALL,
+		self,
+		true,
+	)
 	# NOTE: commit_action() is called immediately after.
-	# NOTE: Atlas is guaranteed to have only been auto-generated with no extra peering bit information.
+	# NOTE: Atlas is guaranteed to have only been auto-generated
+	# with no extra peering bit information.
 	TerrainPreset.write_default_preset(urm, tile_set, atlas)
 	urm.commit_action()
